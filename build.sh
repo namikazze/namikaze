2d8b1ae6f691aee4d55a9a978b6cce72301a0f0f
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-02-10 17:29:47 +0700

    whyred: build: BananaDroid Tanduk

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/namikazze/local_manifest --depth 1 -b banana-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/namikazze/local_manifest --depth 1 -b banana-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -8,14 +8,14 @@ source $CIRRUS_WORKING_DIR/script/config
 timeStart
 
 source build/envsetup.sh
-export BUILD_USERNAME=namikazze
+export BUILD_USERNAME=namikaze
 export BUILD_HOSTNAME=android-build
 lunch banana_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-m banana > reading #& sleep 95m # Jangan di hapus text line (> reading)
+m banana -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
