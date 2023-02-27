35bc7046d02c868761cb940b45524ecf4c754a42
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-02-27 17:32:40 +0700

    whyred: build: Havoc

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Evolution-X/manifest -b snow -g default,-mips,-darwin,-notdefault
-git clone https://github.com/namikazze/local_manifest --depth 1 -b evo-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Havoc-OS/android_manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/namikazze/local_manifest --depth 1 -b havoc-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,12 +10,12 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
-lunch evolution_whyred-userdebug
+lunch evolution_whyred-user
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka evolution -j8 > reading #& sleep 95m # Jangan di hapus text line (> reading)
+mka havoc -j8 > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
