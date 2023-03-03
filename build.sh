18676d91eff5dbc48dd7c8be983c7b40d76a04e4
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-03-03 12:28:36 +0700

    whyred: build: LMODroid

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/LMODroid/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/namikazze/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
 git clone https://github.com/namikazze/local_manifest --depth 1 -b lmodroid-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
