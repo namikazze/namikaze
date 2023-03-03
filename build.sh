d5b9cb300c8f261b74d9066b0431c958fd07eb85
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-03-03 07:30:32 +0700

    whyred: build: lmodroid 12

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 12 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/namikazze/local_manifest --depth 1 -b banana-12 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://git.libremobileos.com/LMODroid/manifest.git -b twelve -g default,-mips,-darwin,-notdefault
+git clone https://github.com/namikazze/local_manifest --depth 1 -b lmodroid-12 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -10,12 +10,12 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
-lunch banana_whyred-user
+lunch lmodroid_whyred-user
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-m banana -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
+mka lmodroid -j8 > reading & sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
