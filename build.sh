4fa95cac49c967f677505da7678dd73cc539dfb5
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-03-24 22:19:37 +0700

    whyred: build: lmodroid

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ timeStart
 source build/envsetup.sh
 export BUILD_USERNAME=zacky
 export BUILD_HOSTNAME=android-build
-lunch lmodroid_whyred-user
+lunch lmodroid_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
