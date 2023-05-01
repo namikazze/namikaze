616c819b9ca53fae536a6290c80c1ad3c18ebc68
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-05-01 08:05:38 +0700

    update build

--- build.sh
+++ build.sh
@@ -1,22 +1,21 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/bananadroid/android_manifest.git -b 13 --git-lfs -g default,-mips,-darwin,-notdefault
-git clone https://github.com/namikazze/local_manifest --depth 1 -b banana-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/thedoctorsz/local_manifest --depth 1 -b superior-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source $CIRRUS_WORKING_DIR/script/config
+source $CIRRUS_WORKING_DIR/config
 timeStart
 
 source build/envsetup.sh
-export BUILD_USERNAME=zacky
+export BUILD_USERNAME=nobody
 export BUILD_HOSTNAME=android-build
-export WITH_GAPPS=true
-lunch banana_whyred-userdebug
+lunch superior_whyred-userdebug
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-mka banana -j9 > reading #& sleep 95m # Jangan di hapus text line (> reading)
+m bacon -j9 > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
