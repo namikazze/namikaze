6d7f52b73d09ee13d3f3147243ce99e6fd93c332
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-05-01 18:39:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-m bacon -j8 > reading #& sleep 95m # Jangan di hapus text line (> reading)
+m bacon -j32 > reading #& sleep 95m # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
