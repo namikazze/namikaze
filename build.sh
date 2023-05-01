d4a2423d62f63e3a3eedc707377f87f4b24a3fbe
namikaze みなと <124431255+namikazze@users.noreply.github.com>
2023-05-01 08:21:53 +0700

    build: update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/SuperiorOS/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/thedoctorsz/local_manifest --depth 1 -b superior-13 .repo/local_manifests
+git clone https://github.com/namikazze/local_manifest --depth 1 -b superior-13 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
